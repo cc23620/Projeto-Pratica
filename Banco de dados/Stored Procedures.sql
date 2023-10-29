27e9cc1850eb3deae880a3a5f8120788fd2994c6
cc23527 <cc23527@g.unicamp.br>
2023-10-29 11:56:09 -0300

    Update Stored Procedures.sql

--- Banco de dados/Stored Procedures.sql
+++ Banco de dados/Stored Procedures.sql
@@ -1,62 +1,35 @@
-
--- uso: EXEC AddCliente 'John Doe', 'johndoe@example.com', 'Sports';
-
-CREATE PROCEDURE AddCliente
-    @NomeCliente VARCHAR(60),
-    @Email VARCHAR(100),
-    @TipoFavorito VARCHAR(30)
-AS
+-- uso: call AddCliente ('John Doe', 'johndoe@example.com', 'Sports');
+CREATE PROCEDURE AddCliente(
+    IN NomeCliente VARCHAR(60),
+    IN Email VARCHAR(100),
+    IN TipoFavorito VARCHAR(30)
+)
 BEGIN
-    INSERT INTO cdj.clientes (nome_cliente, email, tipo_favorito)
-    VALUES (@NomeCliente, @Email, @TipoFavorito);
+    INSERT INTO cdjclientes (nome_cliente, email, tipo_favorito)
+    VALUES (NomeCliente, Email, TipoFavorito);
 END;
 
 
---uso: EXEC AddReserva @ClienteID = 1, @DataReserva = '2023-10-10', @Horario = '14:00:00', @QtePessoas = 4;
-CREATE PROCEDURE AddReserva
-    @ClienteID INT,
-    @DataReserva DATE,
-    @Horario TIME,
-    @QtePessoas INT
-AS
+-- uso: call AddReserva (1,'2023-10-10','14:00:00',4);
+CREATE PROCEDURE AddReserva(
+    IN ClienteID INT,
+    IN DataReserva DATE,
+    IN Horario TIME,
+    IN QtePessoas INT
+)
 BEGIN
-    INSERT INTO cdj.reservas (id_cliente, data_reserva, horario, qte_pessoas)
-    VALUES (@ClienteID, @DataReserva, @Horario, @QtePessoas);
+    INSERT INTO cdjreservas (id_cliente, data_reserva, horario, qte_pessoas)
+    VALUES (ClienteID, DataReserva, Horario, QtePessoas);
 END;
 
-
--- stored procedure para atualizar o email dos clientes
-CREATE PROCEDURE [CDJ].[AtualizarEmailCliente]
-    @clienteID INT,
-    @novoEmail VARCHAR(100)
-AS
+-- uso: call  AtualizarEmailCliente (9, 'IloveMoney@attmail.com');
+CREATE PROCEDURE AtualizarEmailCliente(
+    in clienteID INT,
+    in novoEmail VARCHAR(100))
 BEGIN
-    UPDATE [CDJ].[clientes]
+    UPDATE cdjclientes
     SET email = @novoEmail
     WHERE id_cliente = @clienteID;
 END;
 
-
-
--- stored procedure para REMOVER CLIENTES
--- exec removeCliente 1(1 sendo um exemplo para o id do cliente)
-CREATE PROCEDURE removeCliente
-    @ClienteID INT
-AS
-BEGIN
-    DELETE FROM CDJ.clientes
-    WHERE id_cliente = @ClienteID;
-END;
-
-
-
--- stored procedure para REMOVER RESERVAS
---uso: EXEC RemoveReservation @ReservaID = 1; 
-CREATE PROCEDURE RemoveReservation
-    @ReservaID INT
-AS
-BEGIN
-    DELETE FROM CDJ.reservas
-    WHERE id_reserva = @ReservaID;
-END;
 
